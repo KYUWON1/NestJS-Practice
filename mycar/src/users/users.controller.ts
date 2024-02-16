3284cc20bce15f6ba2a5b79b48f054a1e565c651
KYUWON1 <127181209+KYUWON1@users.noreply.github.com>
2024-02-16 16:21:04 +0900

    24.02.16

--- mycar/src/users/users.controller.ts
+++ mycar/src/users/users.controller.ts
@@ -1,5 +1,16 @@
-import { Body, Controller,Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Param,
+  Query,
+  Delete,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth') // /auth/...
@@ -12,4 +23,32 @@ export class UsersController {
     //console.log(body);
     this.userService.create(body.email, body.password);
   }
+
+  @Get('/:id') //주소에서받는 숫자는 string으로 됨
+  async findUser(@Param('id') id:string ){
+    const user = await this.userService.findOne(parseInt(id)); // int로 전환해서 전달 
+    if(!user){
+      throw new NotFoundException('유저 발견 못함');
+    }
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email:string){ // /auth?email= ~~~ 주소로 전달
+    return this.userService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body:UpdateUserDto){
+    return this.userService.update(parseInt(id),body); //Dto와 Id를 전달 body는 id pass ,id, pass 존재
+  }
+
+  @Delete(':id')
+  removeUser(@Param('id') id:string){
+    return this.userService.remove(parseInt(id));
+  }
+
+
+
+
 }
--- mycar/src/users/users.service.ts
+++ mycar/src/users/users.service.ts
@@ -1,18 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import {User} from './user.entity';
+import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
     //의존성주입을 통해 repo와 연동
+    //repo에 존재하는 API : create,save,find,findOne,remove.. 다른것도 있음 문서참조
     constructor(@InjectRepository(User) private repo: Repository<User>){
     }
 
     create(email:string, password:string) {
         //엔티티 instance를 생성
+        //instance를생성하고 데이터베이스에 저장해야 오류가 덜 발생하고, hooks 사용가능함
         const user = this.repo.create({email, password});
         //save메소드를 통해 데이터베이스에 저장함 
         return this.repo.save(user);
+    }
+
+    findOne(id: number){
+        return this.repo.findOneBy({id});
+    }
+
+    find(email:string){
+        //배열로 리턴
+        return this.repo.find({where:{email}});
+    }
+
+    //Partial은 User클래스의 일부, 아무것도 없는 객체가 될수있다고알려줌
+    async update(id:number, attrs:Partial<User>){
+        const user = await this.findOne(id);
+        if(!user){
+            //throw new Error('유저없음'); Nest에선 해당 형태의 에러 받지못함
+            throw new NotFoundException('발견못함');
+        }
+        Object.assign(user,attrs); //attrs에서 모든 값을 가져와 user에 복사하는 메소드
+        return this.repo.save(user);
+    }
+
+    async remove(id:number){
+        const user = await this.findOne(id);
+        if(!user){
+            throw new NotFoundException('발견못함');
+        } 
+        //엔티티를 통한 remove 후크 동작함
+        return this.repo.remove(user);
     }
 }
